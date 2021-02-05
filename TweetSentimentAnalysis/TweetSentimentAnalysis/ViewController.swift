7f7a67d1d394fd852b86504a670d1f33235e08b8
André Cocuroci <andre.martins@picpay.com>
2021-02-05 14:49:08 -0300

    Configuração inicial do projeto

---
+++ TweetSentimentAnalysis/TweetSentimentAnalysis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TweetSentimentAnalysis
+//
+//  Created by André Martins on 05/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
